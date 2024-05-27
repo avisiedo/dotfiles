8d5b4f339079a99d635a3dc9a4098727338c6adb
Alejandro Visiedo <avisiedo@redhat.com>
2024-05-27 16:31:16 +0200

    feat: add nvm profile script
    When nvm is installed in the system, set up the configuration.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

---
+++ .profile.d/nvm.sh
@@ -0,0 +1,10 @@
+#
+# Add nvm configuration
+#
+
+if [ -e ~/.nvm ]; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
