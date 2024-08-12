d29f5ba444b3362a938294f632d4ffdb48757146
Alejandro Visiedo <avisiedo@redhat.com>
2024-08-12 08:14:20 +0200

    feat: add powerline configuration
    This change add configuration to customize powerline
    to align to my preferences.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

---
+++ .profile.d/powerline.sh
@@ -0,0 +1,16 @@
+# https://github.com/justjanne/powerline-go
+# Require: dnf install powerline-go powerline-font
+function _update_ps1() {
+    PS1="$(powerline-go -error $? -jobs $(jobs -p | wc -l) -cwd-max-depth 3 -numeric-exit-codes -theme default)"
+
+    # Uncomment the following line to automatically clear errors after showing
+    # them once. This not only clears the error for powerline-go, but also for
+    # everything else you run in that shell. Don't enable this if you're not
+    # sure this is what you want
+
+    #set "?"
+}
+
+if [ "${TERM}" != "linux" ] && [ -f "$(which powerline-go 2>/dev/null)" ]; then
+    PROMPT_COMMAND="_update_ps1; ${PROMPT_COMMAND}"
+fi
