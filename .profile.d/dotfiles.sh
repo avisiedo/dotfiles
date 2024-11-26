f0b661b7e1f009c282ffa37609c0df96ed2c0f9a
Alejandro Visiedo <avisiedo@redhat.com>
2024-11-26 18:53:41 +0100

    fix: export dotfiles function
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- .profile.d/dotfiles.sh
+++ .profile.d/dotfiles.sh
@@ -1,4 +1,8 @@
-alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+function dotfiles {
+	/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" "$@"
+}
+export -f dotfiles
+
 dotfiles config status.showUntrackedFiles no
 source /usr/share/bash-completion/completions/git
 __git_complete dotfiles __git_main
