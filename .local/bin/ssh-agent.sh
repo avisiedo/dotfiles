105033eb296b5eb13ecccb267422f7c694b60c44
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-06-03 08:44:57 +0200

    refactor: enhance ssh-agent invokation
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

---
+++ .local/bin/ssh-agent.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# see: links "https://rabexec.org/posts/pitfalls-of-ssh-agents"
+
+# @brief start the ssh-agent and use it in a "safe" way.
+start_ssh_agent() {
+    local ret
+
+    ssh-add -l &>/dev/null; ret="$?"
+    [ "$ret" != 2 ] && return $ret
+
+    test -r "${HOME}/.ssh-agent" && \
+        eval "$(<"${HOME}/.ssh-agent")" >/dev/null
+
+    ssh-add -l &>/dev/null; ret="$?"
+    [ "$ret" != 2 ] && return $ret
+
+    (umask 066; ssh-agent > "${HOME}/.ssh-agent")
+    eval "$(<"${HOME}/.ssh-agent")" >/dev/null
+    ssh-add -t 3600
+}
+
+main() {
+    start_ssh_agent
+}
+
+if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+    main "$@"
+fi
+
--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,27 +1,6 @@
 # see: links "https://rabexec.org/posts/pitfalls-of-ssh-agents"
 
-# @brief start the ssh-agent and use it in a safe way.
-start_ssh_agent() {
-    # TODO Keep in mind the notes of the article and 
-    # implement in a safe way
-    :
-}
-
-if which ssh-agent &>/dev/null; then
-	# eval "$(ssh-agent -s)" &>/dev/null
-	# keyfile_list=("$HOME/.ssh/id_ed25519")
-	# for keyfile in "${keyfile_list[@]}"; do
-		# [ ! -e "${keyfile}" ] || ssh-add "$HOME/.ssh/id_ed25519"
-        # :
-	# done
-	# unset keyfile
-	# unset keyfile_list
-
-    # TODO naive approach, read the article about the
-    #      security concerns and use an approach which
-    #      is safer
-    :
-fi
+ssh-agent.sh
 
 export SSH_ASKPASS=/usr/libexec/openssh/ssh-askpass
 
