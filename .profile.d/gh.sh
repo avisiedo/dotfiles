a4b9834d102caefa77ad9dcec1d407dba2562c96
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-06-03 08:52:17 +0200

    fix: check .github.conf permissions
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/gh.sh
+++ .profile.d/gh.sh
@@ -1,3 +1,11 @@
 if which gh &>/dev/null; then
   source <(gh completion -s "$(basename "$SHELL")")
+  # Only load if permissions are 0400
+  [ ! -e ~/.github.conf ] || {
+    if stat ~/.github.conf | grep Access | grep -q 0400; then
+      source ~/.github.conf
+    else
+      printf "error: ~/.github.conf wrong permissions: no 0400\n" >&2
+    fi
+  }
 fi
