0d1f400cb7418697190faaa4735cb10312dafed8
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-06-13 17:50:00 +0200

    chore: add comment about checking a container environment
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/powerline.sh
+++ .profile.d/powerline.sh
@@ -1,6 +1,8 @@
 # https://github.com/justjanne/powerline-go
 # Require: dnf install powerline-go powerline-font
 function _update_ps1() {
+    # For podman we can check if the context is a container by checking
+    # /run/.containerenv
     PS1="$(powerline-go -error $? -jobs $(jobs -p | wc -l) -cwd-max-depth 3 -numeric-exit-codes -theme default)"
 
     # Uncomment the following line to automatically clear errors after showing
