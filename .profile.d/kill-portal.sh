80c1ac59ec3234329a7803c98ffaa2afbbe23fcd
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-06-16 16:40:14 +0200

    fix: reduce time on sleep
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/kill-portal.sh
+++ .profile.d/kill-portal.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(exec 0</dev/null; exec 1>/dev/null; exec 2>/dev/null; while [ 1 -eq 1 ]; do pkill -f xdg-desktop-portal; sleep 1; done) &
+(exec 0</dev/null; exec 1>/dev/null; exec 2>/dev/null; while [ 1 -eq 1 ]; do pkill -f xdg-desktop-portal; sleep 0.5; done) &
