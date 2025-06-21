eeca0896c67f0bce1c8210fd5f18504a32648746
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-06-21 16:42:07 +0200

    refactor: decouple powerline modules
    We indicate the list of modules to activate in a separate variable so
    make it more readable and easier to modify.
    
    It enabled direnv module.
    
    Add documentation from powerline-go -help about the default modules and
    the list of modules available.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/powerline.sh
+++ .profile.d/powerline.sh
@@ -3,7 +3,18 @@
 function _update_ps1() {
     # For podman we can check if the context is a container by checking
     # /run/.containerenv
-    PS1="$(powerline-go -error $? -jobs $(jobs -p | wc -l) -cwd-max-depth 3 -numeric-exit-codes -theme default)"
+    #
+    # (valid choices: aws, bzr, cwd, direnv, docker, docker-context, dotenv,
+    # duration, exit, fossil, gcp, git, gitlite, goenv, hg, host, jobs, kube,
+    # load, newline, nix-shell, node, perlbrew, perms, plenv, rbenv, root, rvm,
+    # shell-var, shenv, ssh, svn, termtitle, terraform-workspace, time, user,
+    # venv, vgo, vi-mode, wsl)
+    # Unrecognized modules will be invoked as 'powerline-go-MODULE' executable
+    # plugins and should output a (possibly empty) list of JSON objects that
+    # unmarshal to powerline-go's Segment structs.
+    # (default "venv,user,host,ssh,cwd,perms,git,hg,jobs,exit,root")
+    local MODULES="direnv,venv,user,host,ssh,cwd,perms,git,jobs,exit,root"
+    PS1="$(powerline-go -error $? -newline -jobs "$(jobs -p | wc -l)" -modules "${MODULES}" -cwd-max-depth 3 -numeric-exit-codes -theme default)"
 
     # Uncomment the following line to automatically clear errors after showing
     # them once. This not only clears the error for powerline-go, but also for
