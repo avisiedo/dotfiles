adf22ad6fb76f5bfa73c4b5b61ccfd210a602f7c
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-06-30 11:17:23 +0200

    fix: typo when starting rhythmbox
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .config/sway/swayinitapp.sh
+++ .config/sway/swayinitapp.sh
@@ -8,9 +8,9 @@ if ! pgrep opensnitch-ui &>/dev/null; then
     fi
 fi
 
-if ! pgrep rythmbox &>/dev/null; then
-    if which rythmbox &>/dev/null; then
-        swaymsg "exec /usr/bin/rythmbox"
+if ! pgrep rhythmbox &>/dev/null; then
+    if which rhythmbox &>/dev/null; then
+        swaymsg "exec /usr/bin/rhythmbox"
     fi
 fi
 
