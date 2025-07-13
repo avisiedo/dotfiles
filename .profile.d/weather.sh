b03784bff99ebfc41537408877d898a496be07cc
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-07-13 09:43:21 +0200

    fix: arp not installed in toolbox
    Avoid the command execution when arp is not available in the
    environment.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/weather.sh
+++ .profile.d/weather.sh
@@ -1,3 +1,5 @@
 if tty -s &>/dev/null; then
-    [ $(arp -n | grep -v Address | wc -l) -eq 0 ] || curl "https://wttr.in"
+    if which arp &>/dev/null; then
+        [ $(arp -n | grep -v Address | wc -l) -eq 0 ] || curl "https://wttr.in"
+    fi
 fi
