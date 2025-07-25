0f36c3e40181e15b60ae4309e50fb1a3b274cd72
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-07-25 05:14:03 +0200

    fix: load ssh-agent on interactive shell
    Only try to load the ssh-agent when we are running an
    interactive shell.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,6 +1,8 @@
 # see: links "https://rabexec.org/posts/pitfalls-of-ssh-agents"
 
-ssh-agent.sh
+if tty -s &>/dev/null; then
+    ssh-agent.sh
+fi
 
 export SSH_ASKPASS=/usr/libexec/openssh/ssh-askpass
 
