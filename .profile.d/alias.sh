807445ac9ed761397414d8a5e54fd551c90bdb1b
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-08-12 16:38:00 +0200

    fix: split alias en two files
    for macos it is defined the alias below:
    lsusb='ioreg -p IOUSB'
    
    for all the below are defined:
    ll='ls -l --color'
    vi='nvim'  # when neovim is installed
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/alias.sh
+++ .profile.d/alias.sh
@@ -1,4 +1,10 @@
+##
+# Alias definitions
+##
 if tty -s &>/dev/null; then
-    alias ll='ls -l --color'
-    alias ssh-add='/usr/bin/ssh-add -t 300'
+  alias ll='ls -l --color'
+  alias ssh-add='/usr/bin/ssh-add -t 300'
+  if [ "" != "$(which nvim 2>/dev/null)" ]; then
+    alias vi='$(which nvim 2>/dev/null)'
+  fi
 fi
