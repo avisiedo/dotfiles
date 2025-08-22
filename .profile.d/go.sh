51ad2c650825881f257f1f6cf9b90231f617d6c7
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-08-22 19:09:24 +0200

    fix: leverage pathmunge and append_path in go.sh
    Update include file for portability between
    Fedora and Arch Linux.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/go.sh
+++ .profile.d/go.sh
@@ -3,7 +3,15 @@
 ##
 if [ -e ~/go ]; then
   # make available the go tools installed in the user environment
-  pathmunge "$HOME/go/bin"
+  if type -t pathmunge &>/dev/null; then
+    # Fedora /etc/profile define pathmunge
+    pathmunge "$HOME/go/bin"
+  elif type -t append_path &>/dev/null; then
+    # Arch linux /etc/profile define append_path
+    append_path "$HOME/go/bin"
+  else
+    PATH="$PATH:$HOME/go/bin"
+  fi
   export PATH
   export GOSUMDB="sum.golang.org"
 fi
