5fe5ec73d35aad98513d35669ddb64f75b57cb6e
Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>
2025-11-18 17:04:00 +0100

    feat: alias for visual studio code
    Define alias 'code' for running visual studio code
    depending on:
    
    - If it is installed by flatpak, then it defines 'code' alias to allow
      the same semantic as we were using directly in the host by using:
      flatpak run com.visualstudio.code
    - If we are inside a toolbox, then it defines 'code' alias to allow
      the same semantic as we were using outside the toolbox by using:
      flatpak-spawn --host flatpak run com.visualstudio.code
    
    Signed-off-by: Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>

--- .profile.d/alias.sh
+++ .profile.d/alias.sh
@@ -1,6 +1,7 @@
 ##
 # Alias definitions
 ##
+
 if tty -s &>/dev/null; then
   alias ll='ls -l --color'
   alias ssh-add='/usr/bin/ssh-add -t 600'
@@ -10,4 +11,9 @@ if tty -s &>/dev/null; then
     alias vi='$(which nvim 2>/dev/null)'
   fi
   alias ramalama-run='ramalama run --pull never --network host deepseek-coder:33b'
+  if flatpak info com.visualstudio.code &>/dev/null; then
+    alias code='flatpak run com.visualstudio.code'
+  elif flatpak-spawn --host flatpak info com.visualstudio.code &>/dev/null; then
+    alias code='flatpak-spawn --host flatpak run com.visualstudio.code'
+  fi
 fi
