8536c13307097d68d7474b57899162a142d8ffbb
Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>
2025-11-19 01:21:21 +0100

    feat: add firefox alias
    Depending on the environment, define
    
    - firefox to be used on flatpak.
    - firefox to be invoked from a toolbox container.
    
    Signed-off-by: Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>

--- .profile.d/alias.sh
+++ .profile.d/alias.sh
@@ -11,9 +11,18 @@ if tty -s &>/dev/null; then
     alias vi='$(which nvim 2>/dev/null)'
   fi
   alias ramalama-run='ramalama run --pull never --network host deepseek-coder:33b'
+
+  # Visual Studio Code
   if flatpak info com.visualstudio.code &>/dev/null; then
     alias code='flatpak run com.visualstudio.code'
   elif flatpak-spawn --host flatpak info com.visualstudio.code &>/dev/null; then
     alias code='flatpak-spawn --host flatpak run com.visualstudio.code'
+  fi
+
+  # Firefox
+  if flatpak info org.mozilla.firefox; then
+    alias firefox='flatpak run org.mozilla.firefox'
+  elif flatpak-spawn --host flatpak info org.mozilla.firefox; then
+    alias firefox='flatpak-spawn --host flatpak run org.mozilla.firefox'
   fi
 fi
