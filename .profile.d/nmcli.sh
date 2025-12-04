8a8c79e3bb13567d4c7c61836cf2878421892732
Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>
2025-12-04 21:01:10 +0100

    fix: remove nmcli.sh script from profiles
    Signed-off-by: Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>

--- .profile.d/nmcli.sh
+++
@@ -1,6 +0,0 @@
-if tty -s && [ -e ~/.local/bin/setup-bridge128 ] && [ ! -e /var/run/.containerenv ]; then
-  if ! nmcli device show bridge128 | grep IP4.ADDRESS | grep -q -v -- --; then
-    echo "Setting up bridge128" >&2
-    ~/.local/bin/setup-bridge128
-  fi
-fi
