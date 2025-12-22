d439fc81626aab14a96b895e0a90494fbb3f285d
Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>
2025-12-22 08:49:30 +0100

    fix: disable ssh-agent
    In a passwordless environment, we won't need ssh-agent.
    
    Signed-off-by: Alejandro Visiedo <learning.little.engineer.2025.1@gmail.com>

--- .profile.d/ssh.sh
+++ .profile.d/ssh.sh
@@ -1,9 +1,11 @@
 # see: links "https://rabexec.org/posts/pitfalls-of-ssh-agents"
 
-if tty -s &>/dev/null; then
-    if which ssh-agent.sh &>/dev/null; then
-        source "$(which "ssh-agent.sh")"
-        ssh_agent_start
+if false; then
+    if tty -s &>/dev/null; then
+        if which ssh-agent.sh &>/dev/null; then
+            source "$(which "ssh-agent.sh")"
+            ssh_agent_start
+        fi
     fi
 fi
 
