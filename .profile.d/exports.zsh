b88083e81388cd03339f936f0e21d44363920cf2
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2026-02-24 14:33:27 +0100

    fix: add /opt/homebrew/bin to PATH
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/exports.zsh
+++ .profile.d/exports.zsh
@@ -1,1 +1,11 @@
-exports.sh
+if tty -s &>/dev/null; then
+  if which nvim &>/dev/null; then
+    export EDITOR=/usr/bin/neovim
+  elif which vim &>/dev/null; then
+    export EDITOR=/usr/bin/vim
+  elif which vi &>/dev/null; then
+    export EDITOR=/usr/bin/vi
+  fi
+fi
+
+export PATH="$HOME/opt/bin:$PATH"
