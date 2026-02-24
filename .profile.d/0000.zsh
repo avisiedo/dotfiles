c0be98b88980b9eb807485b8f086c7d2ee61c447
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2026-02-24 14:33:27 +0100

    fix: add /opt/homebrew/bin to PATH
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/0000.zsh
+++ .profile.d/0000.zsh
@@ -2,6 +2,8 @@ export_path() {
     ! tty -s &>/dev/null && return 0
     [ ! -e /opt/homebrew/bin/ssh ] && return 0
 
-    export PATH="~/opt/bin:$PATH"
+    export PATH="/opt/homebrew/bin:$PATH"
     return 0
 }
+
+export_path
