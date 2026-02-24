38491d3d8a6a5ff1f51f26f3abd91eae53312624
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2026-02-24 13:17:40 +0100

    fix: the nvim alias set incorrectly in macos

--- .profile.d/alias.zsh
+++ .profile.d/alias.zsh
@@ -3,7 +3,7 @@
 ##
 
 alias ll='ls -l --color'
-if [ "" != "$(which nvim 2>/dev/null)" ]; then
+if which -s nvim &>/dev/null; then
   alias vi='$(which nvim 2>/dev/null)'
 fi
 alias lsusb='ioreg -p IOUSB'
