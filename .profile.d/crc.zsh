bc87799e8ea5e7e20994985f2c295c6a04f58810
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2026-03-01 12:46:16 +0100

    fix: crc oc-env and crc completion
    - Add check by 'crc status' before include the environment and
      completion. In other case, we get an error when including
      the environment for 'oc'
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/crc.zsh
+++ .profile.d/crc.zsh
@@ -1,6 +1,8 @@
 if which -s crc &>/dev/null; then
-  source <(crc oc-env)
-  source <(crc completion $(basename "$SHELL"))
+  if crc status &>/dev/null; then
+    source <(crc oc-env)
+    source <(crc completion $(basename "$SHELL"))
+  fi
 fi
 
 if which -s oc &>/dev/null; then
