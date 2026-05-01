e1930be0abd3f040952fd721134bbea390507039
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2026-05-01 18:59:16 +0200

    fix: java_home on macos through homebrew
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- .profile.d/java.zsh
+++ .profile.d/java.zsh
@@ -1,5 +1,5 @@
 if tty -s; then
-    if [ "Darwin" == "$(uname -s)" ]; then
+    if [ "Darwin" = "$(uname -s)" ]; then
         if [ -e "/opt/homebrew/opt/openjdk" ]; then
             export JAVA_HOME="/opt/homebrew/opt/openjdk"
             export PATH="$PATH:${JAVA_HOME}/bin"
